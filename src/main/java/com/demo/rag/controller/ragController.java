c09cd5dbfdc184a52cf57debc4956c5c9d3898a5
santhoshj29 <santhoshjonnadhula269@gmail.com>
2026-03-31 18:22:19 -0500

    Created interactive ShellCommanda and endpoint for the same interaction aswell

--- src/main/java/com/demo/rag/controller/ragController.java
+++ src/main/java/com/demo/rag/controller/ragController.java
@@ -6,18 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.demo.rag.service.responseServiceByPgVS;
 
 import com.demo.rag.service.ragService;
 
 @RestController
-@RequestMapping("/olympics")
+@RequestMapping("/api")
 public class ragController {
 
     private final ragService ragService;
+    private final responseServiceByPgVS responseServiceByPgVS;
 
     @Autowired
-    public ragController(ragService ragService) {
+    public ragController(ragService ragService, responseServiceByPgVS responseServiceByPgVS) {
         this.ragService = ragService;
+        this.responseServiceByPgVS = responseServiceByPgVS;
     }
 
 
@@ -25,6 +28,12 @@ public class ragController {
     public ResponseEntity<String> faq(@RequestParam(value = "message", defaultValue = "How Can I Buy Tickets for the Olympics 2024?") String message){
         
         return ResponseEntity.ok(ragService.faq(message));
+    }
+
+    @GetMapping("/responsefrompgvs")
+    public ResponseEntity<String> response(@RequestParam(value = "question", defaultValue = "What is Spring Boot?") String question){
+        return ResponseEntity.ok(responseServiceByPgVS.response(question));
+
     }
 
 }
